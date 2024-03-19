7882fd4f19cae9570adb8f2c878f1ca5a3084018
tada-js <justfix010@gmail.com>
2024-03-19 23:54:31 +0900

    :truck: Add : Route Group

---
+++ src/app/(route)/restaurants/_model/index.ts
@@ -0,0 +1,9 @@
+export interface RestaurantType {
+  id: number;
+  name?: string | null;
+  address?: string | null;
+  rodaddress?: string | null;
+  category?: string | null;
+  lng?: string;
+  lat?: string;
+}
