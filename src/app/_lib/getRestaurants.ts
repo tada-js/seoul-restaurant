81e3f51a53ca03cc0b9c9730699d52e6ea1d0ab8
tada-js <justfix010@gmail.com>
2024-03-21 23:47:30 +0900

    :truck: Deleted : Route Group 폴더 이동으로 인한 삭제

--- src/app/_lib/getRestaurants.ts
+++
@@ -1,6 +0,0 @@
-const getRestaurants = async () => {
-  const res = await fetch('/api/restaurants');
-  return res.json();
-};
-
-export default getRestaurants;
--- src/app/restaurants/_model/index.ts
+++
@@ -1,9 +0,0 @@
-export interface RestaurantType {
-  id: number;
-  name?: string | null;
-  address?: string | null;
-  rodaddress?: string | null;
-  category?: string | null;
-  lng?: string;
-  lat?: string;
-}
