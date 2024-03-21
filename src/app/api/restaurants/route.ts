2c23d41a0db6a89582237596276dc16651cdadcb
tada-js <justfix010@gmail.com>
2024-03-21 23:50:01 +0900

    :sparkles: Feat : restaurants route handler

--- src/app/api/restaurants/route.ts
+++ src/app/api/restaurants/route.ts
@@ -1,15 +1,41 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { PrismaClient } from '@prisma/client';
-import { RestaurantType } from 'app/restaurants/_model';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(
-  req: NextApiRequest,
-  res: NextApiResponse<RestaurantType[]>
-) {
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get('id') as string;
+  const page = searchParams.get('page') as string;
+  const limit = searchParams.get('limit') as string;
   const prisma = new PrismaClient();
-  const restaurants = await prisma.restaurant.findMany({
-    orderBy: { id: 'asc' },
-  });
 
-  return Response.json(restaurants);
+  if (page) {
+    const count = await prisma.restaurant.count();
+    const skipPage = parseInt(page) - 1;
+    const restaurants = await prisma.restaurant.findMany({
+      orderBy: { id: 'asc' },
+      take: parseInt(limit),
+      skip: skipPage * 20,
+    });
+    return NextResponse.json(
+      {
+        page: parseInt(page),
+        data: restaurants,
+        totalCount: count,
+        totalPage: Math.ceil(count / 10),
+      },
+      {
+        status: 200,
+      }
+    );
+  } else {
+    const restaurants = await prisma.restaurant.findMany({
+      orderBy: { id: 'asc' },
+      where: {
+        id: id ? parseInt(id) : {},
+      },
+    });
+    return NextResponse.json(id ? restaurants[0] : restaurants, {
+      status: 200,
+    });
+  }
 }
