41e066d568c995cba23b9fd7b12d5765e0b92676
tada-js <justfix010@gmail.com>
2024-03-22 16:43:20 +0900

    :sparkles: Add : Location Store

--- src/app/_store/kakaoMap.ts
+++ src/app/_store/kakaoMap.ts
@@ -1,3 +1,8 @@
+import {
+  DEFAULT_LAT,
+  DEFAULT_LNG,
+  DEFAULT_ZOOM,
+} from 'app/_constants/restaurant';
 import { create } from 'zustand';
 
 interface KaKaoMapState {
@@ -5,9 +10,42 @@ interface KaKaoMapState {
   setKaKaoMap(map: any): void;
 }
 
+interface Location {
+  lat?: string | null;
+  lng?: string | null;
+  zoom?: number;
+}
+
+interface LocationState {
+  location: Location;
+
+  setLocation: (
+    lat: LocationState['location']['lat'],
+    lng: LocationState['location']['lng'],
+    zoom: LocationState['location']['zoom']
+  ) => void;
+}
+
 export const useKaKaoMapStore = create<KaKaoMapState>((set) => ({
   kakaoMap: null,
   setKaKaoMap(kakaoMap) {
     set({ kakaoMap });
+  },
+}));
+
+export const useLocationStore = create<LocationState>((set) => ({
+  location: {
+    lat: DEFAULT_LAT,
+    lng: DEFAULT_LNG,
+    zoom: DEFAULT_ZOOM,
+  },
+  setLocation(lat?: string | null, lng?: string | null, zoom?: number) {
+    set((state) => ({
+      location: {
+        lat: lat ?? state.location.lat,
+        lng: lng ?? state.location.lng,
+        zoom: zoom ?? state.location.zoom,
+      },
+    }));
   },
 }));
