459713ac18268d88f43fe8e5b97ae52550711024
tada-js <justfix010@gmail.com>
2024-04-12 23:53:34 +0900

    :sparkles: Add : wran(prisma-client) 이슈 해결

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,11 +2,9 @@ import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import NaverProvider from 'next-auth/providers/naver';
 import KaKaoProvider from 'next-auth/providers/kakao';
+import prisma from 'app/(feature)/_db/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
 import { Adapter } from 'next-auth/adapters';
-
-const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
   session: {
