c92719b6947efd8e10c46428635eb5a05c8dc5f3
tada-js <justfix010@gmail.com>
2024-04-25 03:58:58 +0900

    :green_heart: Fix : next build type error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en" className={notoSansKr.className}>
